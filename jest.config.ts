5431f1550a96062c31d47a2dab333e56e8478794
“BryceDalbey” <brycedalbey@gmail.com>
2024-07-10 17:46:09 -1000

    jest config change

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
   transform: {},
-  coveragePathIgnorePatterns: ["/node_modules/", "/test"],
+  coveragePathIgnorePatterns: ["/node_modules/"],
 };
 
 export default config;
